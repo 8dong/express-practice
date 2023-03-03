b7549b00557422db718117f653dceeb1aaedca50
8dong <hyde981114@gmail.com>
2023-03-04 03:06:50 +0900

    refactory: 싱글톤 패턴 적용

--- src/app.ts
+++ src/app.ts
@@ -2,26 +2,51 @@ import * as express from 'express';
 
 import catsRouter from './cats/cats.route';
 
-const app: express.Express = express();
+class Server {
+  public app: express.Application;
 
-// logging middleware
-app.use((req, res, next) => {
-  console.log(req.rawHeaders[1]);
-  console.log('logging middleware');
-  next();
-});
+  constructor() {
+    const app: express.Application = express();
+    this.app = app;
+  }
 
-// json middleware
-app.use(express.json());
+  private setRoute() {
+    this.app.use(catsRouter);
+  }
 
-app.use(catsRouter);
+  private setMiddleware() {
+    // logging middleware
+    this.app.use((req, res, next) => {
+      console.log(req.rawHeaders[1]);
+      console.log('logging middleware');
+      next();
+    });
 
-// 404 middleware
-app.use((req, res, next) => {
-  console.log('error middleware');
-  res.send({ error: '404 is not found' });
-});
+    // json middleware
+    this.app.use(express.json());
 
-app.listen(3000, () => {
-  console.log('Server listening on port 3000');
-});
+    // router middleware
+    this.setRoute();
+
+    // 404 middleware
+    this.app.use((req, res, next) => {
+      console.log('error middleware');
+      res.send({ error: '404 is not found' });
+    });
+  }
+
+  public listen() {
+    this.setMiddleware();
+
+    this.app.listen(3000, () => {
+      console.log('Server listening on port 3000');
+    });
+  }
+}
+
+function init() {
+  const server = new Server();
+  server.listen();
+}
+
+init();
